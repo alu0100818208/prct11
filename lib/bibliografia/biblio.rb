a8548a7815d03df53dcebf4664f33bfc62de5b97
alu0100818208 <alu0100818208@ull.edu.es>
2015-11-03 16:10:39 +0000

    Añadiendo código serie

--- lib/bibliografia/biblio.rb
+++ lib/bibliografia/biblio.rb
@@ -1,12 +1,13 @@
 class Bibliografia
 
-     attr_accessor  :autor, :titulo
+     attr_accessor  :autor, :titulo, :serie
      
      #Inicializador
-     def initialize(x1,x2)
+     def initialize(x1,x2,x3)
      
           @titulo = x2
           @autor = x1
+          @serie = x3
           
      end
      
