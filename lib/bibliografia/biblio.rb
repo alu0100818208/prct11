4752e6ae1869a149556b0de35552eb7bf7b583ec
alu0100818208 <alu0100818208@ull.edu.es>
2015-11-03 16:14:40 +0000

    Añadiendo código editorial

--- lib/bibliografia/biblio.rb
+++ lib/bibliografia/biblio.rb
@@ -1,13 +1,14 @@
 class Bibliografia
 
-     attr_accessor  :autor, :titulo, :serie
+     attr_accessor  :autor, :titulo, :serie, :editorial
      
      #Inicializador
-     def initialize(x1,x2,x3)
+     def initialize(x1,x2,x3,x4)
      
           @titulo = x2
           @autor = x1
           @serie = x3
+          @editorial = x4
           
      end
      
