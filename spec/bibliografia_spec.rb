bc7ce2a4fde3ba57472a30975399b05ce8dbfd72
alu0100818208 <alu0100818208@ull.edu.es>
2015-11-03 16:38:31 +0000

    Añadido y probado array para autores

--- spec/bibliografia_spec.rb
+++ spec/bibliografia_spec.rb
@@ -5,13 +5,13 @@ describe Bibliografia do
 
 
    before :each do
-      @biblio = Bibliografia.new("Dave","Programming Ruby 1.9 & 2.0","(The Facets of Ruby)","Pragmatic Bookshelf", "4 Edicion", "(July 7, 2013)","ISBN-13: 978-1937785499")
+      @biblio = Bibliografia.new(["Dave Thomas","Andy Hunt", "Chad Fowler"],"Programming Ruby 1.9 & 2.0","(The Facets of Ruby)","Pragmatic Bookshelf", "4 Edicion", "(July 7, 2013)","ISBN-13: 978-1937785499")
    end
    
    describe "Libro" do
 
       it "El nombre del autor es incorrecto" do
-         @biblio.autor.should eq("Dave")
+         @biblio.autor.should eq(["Dave Thomas","Andy Hunt", "Chad Fowler"])
       end
    end
    
@@ -47,7 +47,7 @@ describe Bibliografia do
       describe "Libro" do
 
       it "La fecha es incorrecta" do
-         @biblio.fecha.should eq(" (July 7, 2013)")
+         @biblio.fecha.should eq("(July 7, 2013)")
       end
    end
    
