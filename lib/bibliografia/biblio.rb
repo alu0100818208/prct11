0aae813d43f14d0c53b4d758d6ad2641a1330ee0
alu0100818208 <alu0100818208@ull.edu.es>
2015-11-03 16:24:52 +0000

    Añadiendo código número ISBN

--- lib/bibliografia/biblio.rb
+++ lib/bibliografia/biblio.rb
@@ -1,9 +1,9 @@
 class Bibliografia
 
-     attr_accessor  :autor, :titulo, :serie, :editorial, :no_edicion, :fecha
+     attr_accessor  :autor, :titulo, :serie, :editorial, :no_edicion, :fecha, :isbn
      
      #Inicializador
-     def initialize(x1,x2,x3,x4,x5,x6)
+     def initialize(x1,x2,x3,x4,x5,x6,x7)
      
           @titulo = x2
           @autor = x1
@@ -11,6 +11,7 @@ class Bibliografia
           @editorial = x4
           @no_edicion = x5
           @fecha = x6
+          @isbn = x7
           
      end
      
