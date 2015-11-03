0c768f91410b7ea7f0701946e77ece0ccce86de2
alu0100815146 <alu0100815146@ull.edu.es>
2015-11-03 16:07:48 +0000

    Añadiendo pruebas Serie

--- spec/bibliografia_spec.rb
+++ spec/bibliografia_spec.rb
@@ -5,7 +5,7 @@ describe Bibliografia do
 
 
    before :each do
-      @biblio = Bibliografia.new("Dave","Programming Ruby 1.9 & 2.0")
+      @biblio = Bibliografia.new("Dave","Programming Ruby 1.9 & 2.0","(The Facets of Ruby)")
    end
    
    describe "Libro" do
@@ -19,10 +19,15 @@ describe Bibliografia do
    describe "Libro" do
 
       it "El titulo es incorrecto" do
-         @biblio.titulo.should eq("Programming Ruby 1.9 & 2.0 ")
+         @biblio.titulo.should eq("Programming Ruby 1.9 & 2.0")
       end
    end
    
-   
+   describe "Libro" do
+
+      it "La serie es incorrecta" do
+         @biblio.serie.should eq("(The Facets of Ruby)")
+      end
+   end
 
 end
