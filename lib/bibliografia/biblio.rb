ea62beb3f3684c086637737c1a07138399874d59
alu0100818208 <alu0100818208@ull.edu.es>
2015-11-03 16:21:44 +0000

    Añadiendo código fecha

--- lib/bibliografia/biblio.rb
+++ lib/bibliografia/biblio.rb
@@ -1,15 +1,16 @@
 class Bibliografia
 
-     attr_accessor  :autor, :titulo, :serie, :editorial, :no_edicion
+     attr_accessor  :autor, :titulo, :serie, :editorial, :no_edicion, :fecha
      
      #Inicializador
-     def initialize(x1,x2,x3,x4, x5)
+     def initialize(x1,x2,x3,x4,x5,x6)
      
           @titulo = x2
           @autor = x1
           @serie = x3
           @editorial = x4
           @no_edicion = x5
+          @fecha = x6
           
      end
      
