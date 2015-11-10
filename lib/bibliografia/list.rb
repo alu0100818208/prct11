5c847e7ec12bc131c232563600062e4f0171368d
alu0100818208 <alu0100818208@ull.edu.es>
2015-11-10 22:44:11 +0000

    Testeo de la extracción del nodo. Fallo por falta de código.

--- lib/bibliografia/list.rb
+++ lib/bibliografia/list.rb
@@ -1,3 +1,7 @@
 module List
      Node = Struct.new(:value, :next)
+     
+     class List
+         
+     end
 end
--- spec/bibliografia_spec.rb
+++ spec/bibliografia_spec.rb
@@ -4,15 +4,26 @@ require "bibliografia/list.rb"
 
 describe Bibliografia do
 
-      biblio = Bibliografia::Biblio.new(["Dave Thomas","Andy Hunt", "Chad Fowler"],"Programming Ruby 1.9 & 2.0","(The Facets of Ruby)","Pragmatic Bookshelf", "4 Edicion", "(July 7, 2013)",["ISBN-13: 978-1937785499", "ISBN-10:1937785491"])
+      @biblio1 = Bibliografia::Biblio.new("Scott Chacon", "Pro Git 2009th Edition", "(Pro)", "Apress", "2009 edition", "(August 27, 2009)",[ "ISBN-13: 978-1430218333", "ISBN-10: 1430218339"])
+      
       
       
       describe List::Node do
+         nodo1 = List::Node.new(@biblio1,nil)
          
          it "#Existe un Nodo de la lista con sus datos y su siguiente" do
-            
+            expect(nodo1.value).to eq(@biblio)
+            expect(nodo1.next).to eq(nil)
          end
       
+      end
+      
+      describe List::List do
+         
+         it "#Se extrae el primer elemento de la lista" do
+            expect(nodo).to eq(nil)
+         end
+         
       end
 
      
