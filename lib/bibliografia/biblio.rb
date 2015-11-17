2b7a9294104fdd7f1a83f5a3f529df1e3b2a0899
alu0100818208 <alu0100818208@ull.edu.es>
2015-11-17 11:00:27 +0000

    Comprobamos que el libro que hemos creado pertenece a la clase Libro y a la jerarquía de Biblio. Fallo por falta de código.

--- lib/bibliografia/biblio.rb
+++ lib/bibliografia/biblio.rb
@@ -1,37 +1,22 @@
 module Bibliografia
      class Biblio
 
-          attr_accessor  :autor, :titulo, :serie, :editorial, :no_edicion, :fecha, :isbn, :salida
+          attr_accessor :autor, :titulo, :fecha, :salida
           
           #Inicializador
-          def initialize(x1,x2,x3,x4,x5,x6,x7)
-          
-               @titulo = x2
-               @autor = x1
-               @serie = x3
-               @editorial = x4
-               @no_edicion = x5
-               @fecha = x6
-               @isbn = x7
-               
+          def initialize(x1,x2,x3)
+               @autor=x1
+               @titulo=x2
+               @fecha=x3
           end
           
           def to_s()
-             @salida = "AUTOR/ES: " + "#{@autor}" + "\n" + "TITULO: " + "#{@titulo}" + "\n" + "SERIE:: " + "#{@serie}" + "\n" + "EDITORIAL: " + "#{@editorial}" + "\n" + "Numero de Edicion: " + "#{@no_edicion}" + "\n" + "FECHA: " + "#{@fecha}" + "\n" + "ISBN: " + "#{@isbn}" 
-             
-              puts @salida
+               
           end
      
      
      end
      
-     class Libro < Biblio
-     
-     end
-
-     class Publicacion < Biblio
-     
-     end
      
 end
 
--- spec/bibliografia_spec.rb
+++ spec/bibliografia_spec.rb
@@ -4,9 +4,9 @@ require "bibliografia/list.rb"
 
 describe Bibliografia do
 
-      @biblio1 = Bibliografia::Biblio.new("Scott Chacon", "Pro Git 2009th Edition", "(Pro)", "Apress", "2009 edition", "(August 27, 2009)",[ "ISBN-13: 978-1430218333", "ISBN-10: 1430218339"])
-      @aux1 = Bibliografia::Biblio.new("Scott Chacon", "Pro Git 2009th Edition", "(Pro)", "Apress", "2009 edition", "(August 27, 2009)",[ "ISBN-13: 978-1430218333", "ISBN-10: 1430218339"])
-      @aux2 = Bibliografia::Biblio.new("Scott Chacon", "Pro Git 2009th Edition", "(Pro)", "Apress", "2009 edition", "(August 27, 2009)",[ "ISBN-13: 978-1430218333", "ISBN-10: 1430218339"])
+      @biblio1 = Bibliografia::Libro.new("Scott Chacon", "Pro Git 2009th Edition", "(Pro)", "Apress", "2009 edition", "(August 27, 2009)",[ "ISBN-13: 978-1430218333", "ISBN-10: 1430218339"])
+      @aux1 = Bibliografia::Libro.new("Scott Chacon", "Pro Git 2009th Edition", "(Pro)", "Apress", "2009 edition", "(August 27, 2009)",[ "ISBN-13: 978-1430218333", "ISBN-10: 1430218339"])
+      @aux2 = Bibliografia::Libro.new("Scott Chacon", "Pro Git 2009th Edition", "(Pro)", "Apress", "2009 edition", "(August 27, 2009)",[ "ISBN-13: 978-1430218333", "ISBN-10: 1430218339"])
 
 
       describe List::Node do
@@ -96,11 +96,11 @@ describe Bibliografia do
       
       describe Bibliografia::Biblio do
          
-         libro1 = Bibliografia::Biblio.new("Scott Chacon", "Pro Git 2009th Edition", "(Pro)", "Apress", "2009 edition", "(August 27, 2009)",[ "ISBN-13: 978-1430218333", "ISBN-10: 1430218339"])
-         libro2 = Bibliografia::Biblio.new(["Dave Thomas","Andy Hunt", "Chad Fowler"],"Programming Ruby 1.9 & 2.0","(The Facets of Ruby)","Pragmatic Bookshelf", "4 Edicion", "(July 7, 2013)",["ISBN-13: 978-1937785499", "ISBN-10:1937785491"])
-         libro3 = Bibliografia::Biblio.new(["David Flanagan", "Yukihiro Matsumoto"], "The Ruby Programming Language",nil, "O’Reilly Media", "1 edition", "(February 4, 2008)", ["ISBN-10: 0596516177", "ISBN-13: 978-0596516178"])
-         libro4 = Bibliografia::Biblio.new(["David Chelimsky", "Dave Astels", "Bryan Helmkamp", "Dan North", "Zach Dennis", "Aslak Hellesoy"], "The RSpec Book: Behaviour Driven Development with RSpec, Cucumber, and Friends", "(The Facets of Ruby)", "Pragmatic Bookshelf", "1 edition", "(December 25, 2010)", ["ISBN-10: 1934356379", "ISBN-13: 978-1934356371"])
-         libro5 = Bibliografia::Biblio.new("Richard E. Silverman", "Git Pocket Guide",nil, "O’Reilly Media", "1 edition", "(August 2, 2013)", ["ISBN-10: 1449325866", "ISBN-13: 978-1449325862"])
+         libro1 = Bibliografia::Libro.new("Scott Chacon", "Pro Git 2009th Edition", "(Pro)", "Apress", "2009 edition", "(August 27, 2009)",[ "ISBN-13: 978-1430218333", "ISBN-10: 1430218339"])
+         libro2 = Bibliografia::Libro.new(["Dave Thomas","Andy Hunt", "Chad Fowler"],"Programming Ruby 1.9 & 2.0","(The Facets of Ruby)","Pragmatic Bookshelf", "4 Edicion", "(July 7, 2013)",["ISBN-13: 978-1937785499", "ISBN-10:1937785491"])
+         libro3 = Bibliografia::Libro.new(["David Flanagan", "Yukihiro Matsumoto"], "The Ruby Programming Language",nil, "O’Reilly Media", "1 edition", "(February 4, 2008)", ["ISBN-10: 0596516177", "ISBN-13: 978-0596516178"])
+         libro4 = Bibliografia::Libro.new(["David Chelimsky", "Dave Astels", "Bryan Helmkamp", "Dan North", "Zach Dennis", "Aslak Hellesoy"], "The RSpec Book: Behaviour Driven Development with RSpec, Cucumber, and Friends", "(The Facets of Ruby)", "Pragmatic Bookshelf", "1 edition", "(December 25, 2010)", ["ISBN-10: 1934356379", "ISBN-13: 978-1934356371"])
+         libro5 = Bibliografia::Libro.new("Richard E. Silverman", "Git Pocket Guide",nil, "O’Reilly Media", "1 edition", "(August 2, 2013)", ["ISBN-10: 1449325866", "ISBN-13: 978-1449325862"])
          
          
          vnodos = []
@@ -117,7 +117,20 @@ describe Bibliografia do
             expect(listita.ini).to eq(vnodos[3])
          end
          
+         context "Libros" do
+            ibroaux = Bibliografia::Libro.new("Richard E. Silverman", "Git Pocket Guide",nil, "O’Reilly Media", "1 edition", "(August 2, 2013)", ["ISBN-10: 1449325866", "ISBN-13: 978-1449325862"])
+            
+            it "#Comprobamos que el libro pertenece a la clase Libro" do
+               expect(libroaux).to be_instance_of(Bibliografia::Libro)
+            end
+            
+            it "#Comprobamos que el libro pertenece a la jerarquía de Biblio" do
+               expect(libroaux).to be_kind_of(Bibliografia::Biblio)
+            end
+            
+         end
+         
       end
-
+      
      
 end
