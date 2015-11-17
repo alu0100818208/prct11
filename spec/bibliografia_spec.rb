1a5415edf7e6d94eabee4276984f6ddc2c80a7ca
alu0100818208 <alu0100818208@ull.edu.es>
2015-11-17 11:23:13 +0000

    Limpiamos bibliografia_spec.rb de los test innecesarios de la práctica anterior.

--- spec/bibliografia_spec.rb
+++ spec/bibliografia_spec.rb
@@ -32,32 +32,9 @@ describe Bibliografia do
          
          listita = List::List.new(nodo1)
          
-         it "#Se extrae el primer elemento de la lista" do
-            expect(listita.extraer_ini()).to eq(nodo1)
-         end
-         
-         it "#Se puede insertar un elemento" do
-            listita.insertar_elemento(nodo1)
-            expect(listita.ini).to eq(nodo1)
-         end
-         
-         it "#Se pueden insertar varios" do
-            listita.insertar_varios(vectornodos)
-            expect(listita.ini).to eq(vectornodos[1])
-         end
-         
          it "#Existe una Lista con su cabeza y su cola" do
             expect(listita.ini).not_to eq(nil)
             expect(listita.fin).not_to eq(nil)
-         end
-         
-         it "#Se puede borrar la lista" do
-            listita.borrar()
-            expect(listita.ini).to eq(nil)
-         end
-         
-         it "#Se puede comprobar que la lista está vacía" do
-            expect(listita.vacia).to eq(true)
          end
          
          context "Recorremos la lista" do
@@ -96,27 +73,6 @@ describe Bibliografia do
       
       describe Bibliografia::Biblio do
          
-         libro1 = Bibliografia::Libro.new("Scott Chacon", "Pro Git 2009th Edition", "(Pro)", "Apress", "2009 edition", "(August 27, 2009)",[ "ISBN-13: 978-1430218333", "ISBN-10: 1430218339"])
-         libro2 = Bibliografia::Libro.new(["Dave Thomas","Andy Hunt", "Chad Fowler"],"Programming Ruby 1.9 & 2.0","(The Facets of Ruby)","Pragmatic Bookshelf", "4 Edicion", "(July 7, 2013)",["ISBN-13: 978-1937785499", "ISBN-10:1937785491"])
-         libro3 = Bibliografia::Libro.new(["David Flanagan", "Yukihiro Matsumoto"], "The Ruby Programming Language",nil, "O’Reilly Media", "1 edition", "(February 4, 2008)", ["ISBN-10: 0596516177", "ISBN-13: 978-0596516178"])
-         libro4 = Bibliografia::Libro.new(["David Chelimsky", "Dave Astels", "Bryan Helmkamp", "Dan North", "Zach Dennis", "Aslak Hellesoy"], "The RSpec Book: Behaviour Driven Development with RSpec, Cucumber, and Friends", "(The Facets of Ruby)", "Pragmatic Bookshelf", "1 edition", "(December 25, 2010)", ["ISBN-10: 1934356379", "ISBN-13: 978-1934356371"])
-         libro5 = Bibliografia::Libro.new("Richard E. Silverman", "Git Pocket Guide",nil, "O’Reilly Media", "1 edition", "(August 2, 2013)", ["ISBN-10: 1449325866", "ISBN-13: 978-1449325862"])
-         
-         
-         vnodos = []
-         vnodos[0] = List::Node.new(libro2,nil)
-         vnodos[1] = List::Node.new(libro3,nil)
-         vnodos[2] = List::Node.new(libro4,nil)
-         vnodos[3] = List::Node.new(libro5,nil)
-         vnodosaux = List::Node.new(libro1,nil)
-         
-         listita = List::List.new(@vnodosaux);
-         
-          it "#Insertamos todas las referencias de libros" do
-            listita.insertar_varios(vnodos)
-            expect(listita.ini).to eq(vnodos[3])
-         end
-         
          context "Libros" do
             libroaux = Bibliografia::Libro.new("Richard E. Silverman", "Git Pocket Guide",nil, "O’Reilly Media", "1 edition", "(August 2, 2013)", ["ISBN-10: 1449325866", "ISBN-13: 978-1449325862"])
             
@@ -142,8 +98,7 @@ describe Bibliografia do
             end
             
          end
-         
-         
+   
       end
       
      
