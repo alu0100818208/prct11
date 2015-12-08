2f0199b2096ea32e33988cd9d6b1cd64efe97c2b
alu0100818208 <alu0100818208@ull.edu.es>
2015-12-08 08:31:20 +0000

    Practica 10

--- lib/bibliografia/biblio.rb
+++ lib/bibliografia/biblio.rb
@@ -3,13 +3,15 @@ module Bibliografia
      
           include Comparable
           
-          attr_accessor :autor, :titulo, :fecha, :salida
+          attr_accessor :autor, :titulo, :fecha, :s
           
           #Inicializador
           def initialize(x1,x2,x3)
-               @autor=x1
-               @titulo=x2
-               @fecha=x3
+               
+               @nombre=x1
+               @fecha=x2
+               @titulo=x3
+               
           end
           
           def <=>(anOther)
@@ -32,39 +34,72 @@ module Bibliografia
      end
      
      class Libro < Biblio
-          attr_accessor  :serie, :editorial, :no_edicion, :isbn
+          attr_accessor :edicion, :volumen, :lpublicacion, :editor
           def initialize(x1,x2,x3,x4,x5,x6,x7)
           
-               @titulo = x2
                @autor = x1
-               @serie = x3
-               @editorial = x4
-               @no_edicion = x5
-               @fecha = x6
-               @isbn = x7
+               @fecha = x2
+               @titulo = x3
+               @edicion = x4
+               @volumen = x5
+               @lpublicacion = x6
+               @editor = x7
                
           end
           
           def to_s()
-             @salida = "AUTOR/ES: " + "#{@autor}" + "\n" + "TITULO: " + "#{@titulo}" + "\n" + "SERIE:: " + "#{@serie}" + "\n" + "EDITORIAL: " + "#{@editorial}" + "\n" + "Numero de Edicion: " + "#{@no_edicion}" + "\n" + "FECHA: " + "#{@fecha}" + "\n" + "ISBN: " + "#{@isbn}" 
-             
-              puts @salida
+            
           end
           
      end
      
-     class Publicacion < Biblio
-          attr_accessor :periodico, :tipo, :url
+     class Articulo < Biblio
+          attr_accessor :eds, :titulob, :pags, :edicion, :volumen, :lpublicacion, :editor
           
-          def initialize(x1,x2,x3,x4,x5,x6)
+          def initialize(x1,x2,x3,x4,x5,x6,x7,x8,x9,x10)
           
-               @titulo = x2
                @autor = x1
-               @periodico = x3
-               @fecha = x4
-               @tipo = x5
-               @url =x6
+               @fecha = x2
+               @titulo = x3
+               @eds = x4
+               @titulob = x5
+               @pags = x6
+               @edicion = x7
+               @volumen =x8
+               @lpublicacion = x9
+               @editor = x10
                
+          end
+     end
+     
+     class ArticuloPeriodico < Biblio
+          attr_accessor :periodico, :paginas
+          
+          def initialize(x1,x2,x3,x4,x5)
+          
+               @autor = x1
+               @fecha = x2
+               @titulo = x3
+               @periodico = x4
+               @paginas = x5
+               
+          end
+     end
+     
+     class Electronico < Biblio
+          attr_accessor :edicion, :tmedio, :lpublicacion, :editor, :url, :fechac
+          
+          def initialize(x1,x2,x3,x4,x5,x6,x7,x8,x9)
+          
+               @autor = x1
+               @fecha = x2
+               @titulo = x3
+               @edicion = x4
+               @tmedio = x5
+               @lpublicacion = x6
+               @editor = x7
+               @url = x8
+               @fechac = x9
                
           end
      end
