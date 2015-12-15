1d4c65ed1533891f7eb970f4739c3030b8698d83
alu0100818208 <alu0100818208@ull.edu.es>
2015-12-15 23:26:06 +0000

    Prueba del uso del APA en la clase libro. Fallo por falta de código.

--- spec/bibliografia_spec.rb
+++ spec/bibliografia_spec.rb
@@ -6,5 +6,29 @@ require "bibliografia/list.rb"
 
 describe "Práctica 10" do
      
+      context "Libro" do
+       
+         l = Libro.new("AAAA") do
+            
+            author "A. A." 
+            date "2020"
+            edition "5ª edición"
+            volume "vol 1"
+            place "Marte"
+            editor "Salamandra"
       
+         end
+        
+         it "Se usa el APA en el libro" do
+            expect(l.titulo).to eq("AAAA")
+            expect(l.autor).to eq(["A. A."])
+            expect(l.fecha).to eq(["2020"])
+            expect(l.edicion).to eq(["5ª edición"])
+            expect(l.volumen).to eq(["vol 1"])
+            expect(l.lpublicacion).to eq(["Marte"])
+            expect(l.edito).to eq(["Salamandra"])
+            
+         end
+         
+      end
 end
