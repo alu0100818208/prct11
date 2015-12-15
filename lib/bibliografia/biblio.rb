8fff8132a9e8600521988a5a74d62c1f62898ddd
alu0100818208 <alu0100818208@ull.edu.es>
2015-12-15 23:28:58 +0000

    Prueba del uso del APA en la clase Articulo. Funciona correctamente.

--- lib/bibliografia/biblio.rb
+++ lib/bibliografia/biblio.rb
@@ -106,3 +106,71 @@ class Libro < Biblio
      end
      
 end
+
+class Articulo < Biblio
+
+     attr_accessor :eds, :titulob, :pags, :edicion, :volumen, :lpublicacion, :edito
+     def initialize(titulo,&block)
+     
+          self.titulo = titulo
+          self.autor = []
+          self.fecha = []   
+          self.titulob = []
+          self.pags = []
+          self.edicion = []
+          self.volumen = []
+          self.lpublicacion = []
+          self.edito = []
+          
+          
+          if block_given?  
+               if block.arity == 1
+                    yield self
+               else
+                    instance_eval &block 
+               end
+          end
+          
+     end
+     
+     def date(name, options = {})
+          date = name
+          fecha << date
+     end
+     
+     def author(name, options = {})
+          author = name
+          autor << author
+     end
+     
+     def edition(name, options = {})
+          edition = name
+          edicion << edition
+     end
+     
+     def pages(name, options = {})
+          pages = name
+          pags << pages
+     end
+     
+     def book(name, options = {})
+          book = name
+          titulob << book
+     end
+     
+     def volume(name, options = {})
+          volume = name
+          volumen << volume
+     end
+     
+     def place(name, options = {})
+          place = name
+          lpublicacion << place
+     end
+     
+     def editor(name, options = {})
+          editor = name
+          edito << editor
+     end
+     
+end
