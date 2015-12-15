069aeacc7777b03bb25cd970ac8d7702eec6f8e5
alu0100818208 <alu0100818208@ull.edu.es>
2015-12-15 23:27:00 +0000

    Prueba del uso del APA en la clase libro. Funciona correctamente.

--- lib/bibliografia/biblio.rb
+++ lib/bibliografia/biblio.rb
@@ -1,3 +1,108 @@
 class Biblio
 
+     include Comparable
+     attr_accessor :titulo, :autor, :fecha 
+     
+     def initialize(titulo, &block)
+     
+          self.titulo = titulo
+          self.autor = []
+          self.fecha = []
+     
+          if block_given?  
+               if block.arity == 1
+                    yield self
+               else
+                    instance_eval &block 
+               end
+          end
+     end
+     
+     def date(name, options = {})
+          date = name
+          
+          fecha << date
+     end
+     
+     def author(name, options = {})
+          author = name
+          autor << author
+     end
+     
+     def <=>(anOther)
+          if((@autor <=> anOther.autor) == 0)
+               if((@fecha <=> anOther.fecha)==0)
+                    
+                    @titulo <=> anOther.titulo
+                
+               else
+                    @fecha <=> anOther.fecha
+               end
+          else
+               @autor <=> anOther.autor
+          end
+     end
+     
+     
+     def to_s()
+
+     end
+     
+     
+end
+
+class Libro < Biblio
+     
+     attr_accessor :edicion, :volumen, :lpublicacion, :edito
+     
+     def initialize(titulo,&block)
+         
+          self.titulo = titulo
+          self.autor = []
+          self.fecha = []
+          self.edicion = []
+          self.volumen = []
+          self.lpublicacion = []
+          self.edito = []
+          
+          
+          if block_given?  
+               if block.arity == 1
+                    yield self
+               else
+                    instance_eval &block 
+               end
+          end
+     end
+     
+     def date(name, options = {})
+          date = name
+          fecha << date
+     end
+     
+     def author(name, options = {})
+          author = name
+          autor << author
+     end
+     
+     def edition(name, options = {})
+          edition = name
+          edicion << edition
+     end
+     
+     def volume(name, options = {})
+          volume = name
+          volumen << volume
+     end
+     
+     def place(name, options = {})
+          place = name
+          lpublicacion << place
+     end
+     
+     def editor(name, options = {})
+          editor = name
+          edito << editor
+     end
+     
 end
